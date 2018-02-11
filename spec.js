b0a887ae5dbeadefb974ea2dc8c9c3d8cada47c8
shinytang6 <1074461480@qq.com>
2018-02-11 22:08:50 +0800

    test ci

--- spec.js
+++ spec.js
@@ -3,7 +3,7 @@ const add = require('./add')
 
 describe('add function', () =>{
     it('should return 2 when 1 +  1', (next) =>{
-        expect(add(1,1)).toEqual(2)
-        next()
+        expect(add(1,1)).toEqual(2);
+        next();
     })
 })
